b68332f5d306a711cbac9b63f26437b35a846390
Pavandube97 <pavan.dube@freshcodes.net>
2025-07-24 18:30:39 +0530

    some changes

--- app.js
+++ app.js
@@ -40,5 +40,5 @@
     // Server Start
     const PORT = process.env.PORT || 9000;
     app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    // console.log(`Server running at http://localhost:${PORT}`);
     });
